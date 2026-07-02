00aecccd052561a8a2ec7b36f99a4ae98e8a95e3
fernandez <fernandxx08@gmail.com>
2026-07-02 09:38:08 -0500

    este si es el que va a funcionar creo

--- Sena/config/settings.py
+++ Sena/config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$u@kk!ne+y*gg=^q)9t+cmi%8@9mo3@@$t&a1ab*_tld%61#k#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
